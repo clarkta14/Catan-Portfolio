3b3fb78aff0be951fa47f70d66bdded74552ceb1
Indresh <srivasiscode@gmail.com>
2020-03-31 12:27:00 -0500

    players can create settlement objects and store all their settlements

--- Catan/src/main/java/objects/Player.java
+++ Catan/src/main/java/objects/Player.java
@@ -1,16 +1,28 @@
 package objects;
 
 import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
 
 public class Player {
-	Color color;
-
+	private Color color;
+	private ArrayList<Settlement> settlements;
+	
 	public Player(Color color) {
 		this.color = color;
+		this.settlements = new ArrayList<>();
 	}
 
 	public Object getColor() {
 		return this.color;
+	}
+	
+	public void addSettlement(Point loc) {
+		this.settlements.add(new Settlement(loc));
+	}
+
+	public ArrayList<Settlement> getSettlements() {
+		return this.settlements;
 	}
 
 }
--- Catan/src/test/java/objects/PlayerTests.java
+++ Catan/src/test/java/objects/PlayerTests.java
@@ -3,6 +3,7 @@ package objects;
 import static org.junit.Assert.*;
 
 import java.awt.Color;
+import java.awt.Point;
 
 import org.junit.Test;
 
@@ -14,8 +15,6 @@ public class PlayerTests {
 		assertEquals(Color.orange, player.getColor());
 	}
 	
-	
-	
 	@Test
 	public void construct2PlayerDifferentColors() {
 		Player player1 = new Player(Color.orange);
@@ -25,6 +24,22 @@ public class PlayerTests {
 		assertEquals(Color.blue, player2.getColor());
 	}
 	
+	@Test
+	public void addSettlement() {
+		Player player1 = new Player(Color.orange);
+		Point loc = new Point(1,1);
+		player1.addSettlement(loc);
+		assertEquals(loc, player1.getSettlements().get(0).getLocationOnBoard());
+	}
 	
-
+	@Test
+	public void addMultipleSettlements() {
+		Player player1 = new Player(Color.orange);
+		Point loc = new Point(1,1);
+		player1.addSettlement(loc);
+		assertEquals(loc, player1.getSettlements().get(0).getLocationOnBoard());
+		Point loc2 = new Point(2,2);
+		player1.addSettlement(loc2);
+		assertEquals(loc2, player1.getSettlements().get(1).getLocationOnBoard());
+	}
 }
