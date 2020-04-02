9f24e15ace81b05b7de616c2f7e6c346abd4c65c
evansjt <evansjt@rose-hulman.edu>
2020-04-01 20:16:54 -0400

    Wrote tests for getting number of players.

--- Catan/src/main/java/gui/SetupWindow.java
+++ Catan/src/main/java/gui/SetupWindow.java
@@ -3,6 +3,9 @@ package gui;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,8 +16,11 @@ import javax.swing.SpringLayout;
 
 public class SetupWindow {
 	
+	public List<String> playerNames;
+	
 	public SetupWindow() {
-		showGUI();
+//		showGUI();
+		playerNames = new ArrayList<String>();
 	}
 
 
@@ -99,5 +105,33 @@ public class SetupWindow {
 		});
 		
 		instructionsPanel.add(startButton);
+	}
+	
+	public void scanForInput() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many players in this game?");
+		int numPlayers = getPlayerNum(input.nextLine());
+		
+		for (int i = 1; i <= numPlayers; i++) {
+			input = new Scanner(System.in);
+			System.out.println("What is the name of Player " + i);
+			String playerName = input.nextLine();
+			createPlayer(playerName);
+		}
+	}
+	
+	public void createPlayer(String playerName) {
+		this.playerNames.add(playerName);
+	}
+
+
+	public int getPlayerNum(String numPlayersString) {
+		if(numPlayersString == "3") {
+			return 3;
+		} else if(numPlayersString == "4") {
+			return 4;
+		} else {
+			throw new IllegalArgumentException("numPlayers must either be intergers 3 or 4");
+		}
 	}
 }
--- Catan/src/test/java/gui/SetupWindowTests.java
+++ Catan/src/test/java/gui/SetupWindowTests.java
@@ -11,6 +11,66 @@ public class SetupWindowTests {
 		SetupWindow setupWindow = new SetupWindow();
 	}
 	
+	@Test
+	public void testThreePlayers() {
+		SetupWindow catanSetup = new SetupWindow();
+		String three = "3";
+		int players = catanSetup.getPlayerNum(three);
+		assertEquals(3, players);
+	}
+	
+	@Test
+	public void testFourPlayers() {
+		SetupWindow catanSetup = new SetupWindow();
+		String four = "4";
+		int players = catanSetup.getPlayerNum(four);
+		assertEquals(4, players);
+	}
+	
+	@Test
+	public void testLessThanThreePlayers() {
+		try {
+			SetupWindow catanSetup = new SetupWindow();
+			String two = "2";
+			int players = catanSetup.getPlayerNum(two);
+			fail("Did not throw IAE Exception");
+		} catch(IllegalArgumentException e) {
+			// pass
+		}
+	}
+	
+	@Test
+	public void testGreaterThanFourPlayers() {
+		try {
+			SetupWindow catanSetup = new SetupWindow();
+			String five = "5";
+			int players = catanSetup.getPlayerNum(five);
+			fail("Did not throw IAE Exception");
+		} catch(IllegalArgumentException e) {
+			// pass
+		}
+	}
+	
+	@Test
+	public void testNonNumericalForPlayerNum() {
+		try {
+			SetupWindow catanSetup = new SetupWindow();
+			String nonNum = "henlo wurld";
+			int players = catanSetup.getPlayerNum(nonNum);
+			fail("Did not throw IAE Exception");
+		} catch(IllegalArgumentException e) {
+			// pass
+		}
+	}
+	
+	@Test
+	public void testCreatePlayer() {
+		SetupWindow catanSetup = new SetupWindow();
+		String player1 = "John Doe";
+		catanSetup.createPlayer(player1);
+		assertEquals(player1, catanSetup.playerNames.get(0));
+	}
+
 	
 
 }
