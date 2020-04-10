ed886538bea64fdf4294f98a9b1c444a3f8dbc4e
TheCampingCam <cpj1337@gmail.com>
2020-04-09 20:01:04 -0400

    Added RoadTests.java

---
+++ Catan/src/test/java/objects/RoadTests.java
@@ -0,0 +1,14 @@
+package objects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoadTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
