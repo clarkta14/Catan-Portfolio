d8ac1a451a6fb90fb0fe5d8c4c6e7ea08d209784
TheCampingCam <cpj1337@gmail.com>
2020-04-10 00:01:26 -0400

    testConstructor1 passes

--- Catan/src/test/java/objects/RoadTests.java
+++ Catan/src/test/java/objects/RoadTests.java
@@ -2,13 +2,17 @@ package objects;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
 
 public class RoadTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor1() {
+		Player player1 = new Player(Color.BLUE);
+		Road player1Road = new Road(player1);
+		assertEquals(player1, player1Road.getOwner());
 	}
 
 }
