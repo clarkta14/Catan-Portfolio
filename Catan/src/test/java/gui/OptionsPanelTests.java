52d3a34b9058d45609dee6f225b5e108b18f8bd6
clarkta14 <54959325+clarkta14@users.noreply.github.com>
2020-04-14 18:18:29 -0400

    testEndTurnListenerNonSetup4Players

--- Catan/src/test/java/gui/OptionsPanelTests.java
+++ Catan/src/test/java/gui/OptionsPanelTests.java
@@ -47,5 +47,45 @@ public class OptionsPanelTests {
 		
 		EasyMock.verify(bw, gw);
 	}
+	
+	@Test
+	public void testEndTurnListenerNonSetup4Players() {
+		BoardWindow bw = EasyMock.partialMockBuilder(BoardWindow.class)
+				.addMockedMethod("getState")
+				.createMock();
+		GameWindow gw = EasyMock.partialMockBuilder(GameWindow.class)
+				.addMockedMethod("getPlayersController")
+				.addMockedMethod("getBoardWindow")
+				.createMock();
+		
+		PlayersController pc = new PlayersController(4);
+		
+		// put it in not the setup phase
+		for(int i = 0; i < 8; i++) {
+			pc.nextPlayer();
+		}
+
+		EasyMock.expect(gw.getPlayersController()).andStubReturn(pc);
+		EasyMock.expect(gw.getBoardWindow()).andStubReturn(bw);
+		EasyMock.expect(bw.getState()).andStubReturn(GUIStates.idle);
+		
+		EasyMock.replay(bw, gw);
+		
+		OptionsPanel op = new OptionsPanel(gw);
+		EndTurnListener etl = op.new EndTurnListener();
+		
+		
+		assertEquals(0, pc.getCurrentPlayerNum());
+		etl.actionPerformed(null);
+		assertEquals(1, pc.getCurrentPlayerNum());
+		etl.actionPerformed(null);
+		assertEquals(2, pc.getCurrentPlayerNum());
+		etl.actionPerformed(null);
+		assertEquals(3, pc.getCurrentPlayerNum());
+		etl.actionPerformed(null);
+		assertEquals(0, pc.getCurrentPlayerNum());
+		
+		EasyMock.verify(bw, gw);
+	}
 
 }
