c0e19c7b378c74e88a32844dc9209e81549bee09
Indresh <srivasiscode@gmail.com>
2020-04-23 10:58:26 -0500

    Refactored out comments

--- Catan/src/main/java/gui/GameWindow.java
+++ Catan/src/main/java/gui/GameWindow.java
@@ -14,8 +14,8 @@ public class GameWindow {
 	private PlayersController turnController;
 	private CatanBoard catanBoard;
 	private BoardWindow boardWindow;
-    private OptionsPanel options; // To hold buttons for game options
-    private PlayerInfo playerInfo; // Hold all player information
+    private OptionsPanel options;
+    private PlayerInfo playerInfo;
     
     public GameWindow(int numOfPlayers){
     	this.turnController = new PlayersController(numOfPlayers);
@@ -30,12 +30,10 @@ public class GameWindow {
         JFrame frame = new JFrame("Catan");
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	
-        // Dimensions for adding the different components to the screen - CatanBoard, SideBar, etc.
         Dimension dim = new Dimension(5,6);
         Container content = frame.getContentPane();
         content.setLayout(new GraphPaperLayout(dim));
         
-        //Adding components to the window
         content.add(options,new Rectangle(0,0,1,5));
         content.add(boardWindow,new Rectangle(1,0,4,4));
         content.add(playerInfo,new Rectangle(1,4,4,2));
