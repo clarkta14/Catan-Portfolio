db64e1c6ec040fb1c7683b38b769480223282301
Cambron Johnson <cpj1337@gmail.com>
2020-05-20 18:45:03 -0400

    testConstructVertex passes

--- Catan/src/test/java/objects/VertexTests.java
+++ Catan/src/test/java/objects/VertexTests.java
@@ -6,6 +6,14 @@ import org.junit.Test;
 
 public class VertexTests {
 
+	@SuppressWarnings("static-access")
+	@Test
+	public void testConstructVertex() {
+		Vertex vertex = new Vertex();
+		assertEquals(2, vertex.getVertex(3,4));
+	}
+	
+	
 	@Test
 	public void testGetAccurateVertex() {
 		assertEquals(2, Vertex.getVertex(3, 4));
