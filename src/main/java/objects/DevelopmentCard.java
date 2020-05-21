a89caf22e1d674ceb231db15f7467d899af7772c
Indresh Srivastava <srivasiscode@gmail.com>
2020-05-21 18:53:33 -0500

    Resetting for gradle build fix

--- src/main/java/objects/DevelopmentCard.java
+++
@@ -1,8 +0,0 @@
-package objects;
-
-public abstract class DevelopmentCard {
-		
-	public void playCard(TileType... resources) { }
-	
-	public abstract DevelopmentCardType getDevelopmentCardType();
-}
